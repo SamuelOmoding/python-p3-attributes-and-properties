2840ebd8cb3e25b31efa8fe5f72e3097b088316d
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-03-01 11:13:11 +0300

    Assignment done

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+#List of approved dog breeds
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -11,5 +11,39 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+#Dog class definition
 class Dog:
-    pass
+    #Class variable to store the list of approved breeds
+    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+    
+    #Constructor method
+    def __init__(self, name="Ben", breed="Corgi"):
+       #Instance variables initializing with default values
+        self.name = name
+        self.breed = breed
+    #The name property getter and setter methods
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        #To check if the name is a string and its lenght is between 1 and 25 characters
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value
+
+    #The breed property getter and setter methods
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, breed):
+        # Checking if the breed is in the list of approved breeds
+        if breed not in self.approved_breeds:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = breed
+pass
--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+# List of approved jobs
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
@@ -15,5 +15,41 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+# Person class definition
 class Person:
+     # Class variable to store the list of approved jobs
+    approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales",
+                     "General Management", "Research & Development", "Marketing", "Purchasing"]
+
+     # Constructor method
+    def __init__(self, name="Juma", job="Customer Service"):
+        self.name = name
+        self.job = job
+
+    # Getter and setter methods for the 'name' property
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, job):
+        # Checking if the name is a string and its length is between 1 and 25 characters
+        if not isinstance(job, str) or not (1 <= len(job) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = job.title()
+
+    # Getter and setter methods for the 'job' property
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, job):
+        # Checking if the breed is in the list of approved jobs
+        if job not in self.approved_jobs:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = job
+
     pass
